6a0e2ac5ddee080aa759e0b846a1db65134c3911
michael <michael.howard.h@gmail.com>
2021-02-24 12:21:43 +0200

    JDK 11

--- src/test/java/com/thiefspin/jwt/JwtEncoderTest.java
+++ src/test/java/com/thiefspin/jwt/JwtEncoderTest.java
@@ -38,13 +38,4 @@ public class JwtEncoderTest {
         final var str = "Some or other string";
         assertEquals("U29tZSBvciBvdGhlciBzdHJpbmc", JwtEncoder.encodeBase64(str));
     }
-
-    @Test
-    @DisplayName("Sign a HMAC correctly")
-    void testSignHmac() throws JwtException {
-        final var payload = "This is a payload";
-        final var key = "token";
-        final var expectedHmac = "77-977-9EAPvv70HQ--_vWbvv73vv73vv73vv706cEHvv73htJxQdyl877-977-977-9Xe-_ve-_vQnvv70";
-        assertEquals(expectedHmac, JwtEncoder.signHmac(payload, key, Algorithm.HS256));
-    }
 }
